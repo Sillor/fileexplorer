49939df8e727f2fb0152511995e6ac1b03efc805
Rowan Frankenbach <w1933709@apps.losrios.edu>
2022-11-29 10:07:23 -0800

    cleaning some stuff up. its really not doing much at the moment, just printing out the stuff in a directory of your chocie. youll have to change one of the first few lines in main() to make it a directory that actually exists on your computer

--- code/fileexplorer.cpp
+++ code/fileexplorer.cpp
@@ -37,16 +37,12 @@ class Item
 	}
 
 	filesystem::path getDirectoryPath();
-
 	string Item::getDirectoryPathStr();
-
 	void printContents();
-	
 
 	protected:
 		vector<Item*> contents;
 		filesystem::path location;
-
 };
 
 class Directory : public Item
@@ -58,10 +54,10 @@ class Directory : public Item
 	
 		Directory(filesystem::path initPath) : Item(initPath)
 		{
-			//location = initPath;
+			// intentionally blank, can maybe just delete this constructor?
 		}
 
-		void populate();
+		void populate();	// collects the things stored in the directory and saves them in the vector variable "location"
 
 	protected:
 };
@@ -69,45 +65,25 @@ class Directory : public Item
 
 int main()
 {
-	filesystem::path currentDir = "/home/r/Desktop";
-	//cout << currentDir;
-	
-
-	//Directory testDir("/home/r/Desktop");
+	cout << endl << endl << "------------------" << endl << endl;
+	filesystem::path currentDir = "/home/r/Desktop";	// Set this directory to be something on your computer		
 	Directory testDir(currentDir);
 
-	//filesystem::path testPathObj = testDir.getDirectoryPath();
-
-	//cout << endl << testPathObj << endl;
-
-	//Item testItem(currentDir);
-	//testPathObj = testItem.getDirectoryPath();
-	//cout << endl << testPathObj << endl;
-
-
-
-
-
-	
 	testDir.populate();
 	testDir.printContents();
 
-	
+	// The stuff you did is below, im just commenting it out temporarily
+	// -------------------------------
+	/*
 
 
-
-
-
-
-
-	
 	VideoMode vm(1200, 675);
 
 	RenderWindow window(vm, "File Explorer", Style::Close);
 
 	// MAIN LOOP
 	// ##########################################################################################
-	/*
+	
 	while (window.isOpen())
 	{
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
@@ -125,27 +101,26 @@ int main()
 }
 
 
-// function defs
+// FUNCTION DEFS
 // --------------------------------------------------------------------
 
+// collects the things stored in the directory and saves them in the vector member with identifier "location"
 void Directory::populate()
 {
 	
-	for (const filesystem::directory_entry & file : filesystem::directory_iterator(getDirectoryPath()))
-	//for (auto & file : filesystem::directory_iterator(location))		// passing member var 'location' to a <filesystem> iterator which
-																									// generates a filesystem::directory_entry object 
-																									// (its identifier is "file", in the loop condition) for each thing
-																									// in the location as the for loop goes through everything.
-
-																									// The rest of the loop body is adding each thing to the member list
-																									// named "contents".
-	{
-		//cout << getNameFromFullPath(file.path()) << endl;
-		//if (filesystem::is_directory(getNameFromFullPath(file.path())))			// translation: is it a folder itself?
-		if (filesystem::is_directory(file.path()))
+	// passing member var 'location' to a <filesystem> iterator which
+	// generates a filesystem::directory_entry object 
+	// (its identifier is "file", in the loop condition) for each thing
+	// in the location as the for loop goes through everything.
+	//
+	// The rest of the loop body is adding each thing to the member list
+	// named "contents".
+	for (const filesystem::directory_entry & file : filesystem::directory_iterator(getDirectoryPath()))																												
+	{			
+		if (filesystem::is_directory(file.path()))		// translation: is it a folder itself?
 		{
-			Item* tempPtr = nullptr;
-			tempPtr = new Directory(location);								
+			Item* tempPtr = nullptr;								
+			tempPtr = new Directory(file.path());	
 			contents.push_back(tempPtr);
 			//cout << getNameFromFullPath(file.path()) << right << "\t\t\t" << "DIRECTORY ADDED" << endl;
 		}
@@ -153,19 +128,18 @@ void Directory::populate()
 		else if (filesystem::is_symlink(file.path()))
 		{
 			Item* tempPtr = nullptr;
-			tempPtr = new Directory(location);
+			tempPtr = new Directory(file.path());
 			contents.push_back(tempPtr);
 			//cout << "SYM LINK ADDED" << endl;
 		}
 
 		else	// if its anything other than a directory...
 		{
-			Item* tempPtr2 = new Item(location);
+			Item* tempPtr2 = new Item(file.path());
 			contents.push_back(tempPtr2);
 			//cout << getNameFromFullPath(file.path()) << right << "\t\t\t" << "MISCELLANEOUS THING ADDED" << endl;
 		}
 	}
-
 }
 
 filesystem::path Item::getDirectoryPath()
@@ -179,12 +153,9 @@ string Item::getDirectoryPathStr()
 	return holder;
 }
 
-
-
-
 string getNameFromFullPath(string pathParam)
 {
-    string returnVal = "";          // returnVal can be renamed maybe
+    string reversedString = "";          
 
     // Getting the name in reverse
     for (int index = pathParam.length(); index != 0; index--)
@@ -192,7 +163,7 @@ string getNameFromFullPath(string pathParam)
 
         if (pathParam[index] != '/')
         {
-            returnVal = returnVal + pathParam[index];
+            reversedString = reversedString + pathParam[index];
 
         }
         else
@@ -201,36 +172,24 @@ string getNameFromFullPath(string pathParam)
         }
     }
 
-    string returnVal2 = "";
+    string returnVal = "";
     // Putting the name in the correct order
-    for (int index = 0; index < returnVal.length(); index++)
+    for (int index = 0; index < reversedString.length(); index++)
     {
-        returnVal2 = returnVal2 + returnVal[returnVal.length() - index];
+        returnVal = returnVal + reversedString[reversedString.length() - index];
     }
 
 	// TEST OUTPUT
-    //cout << returnVal2 << endl;
+    //cout << returnVal << endl;
 
-    return returnVal2;
+    return returnVal;
 }
 
 void Item::printContents()
 {
-	cout << "test" << endl;
-	for (int index = 0; index < contents.size(); index++)		// not working because loop condition referring to the wrong contents var
-	{
-		cout << "test2";	
+	for (int index = 0; index < contents.size(); index++)		
+	{	
 		string holder = contents[index]->getDirectoryPathStr();
 		cout << holder << endl;
-		//cout << contents[index]->getDirectoryPathStr() << endl;
-		//cout << contents[index]->getDirectoryPath() << endl;
-
-
-		//string* temp = nullptr;
-		//temp = &contents[index]->getDirectoryPath().string();
-		//cout << *temp;
 	}
-
-	//cout << contents[0]->
-
 }
